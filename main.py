0599f3e0aeb94e66bd7c59f3837f3badb76a9225
Sandy Pratama <84855194+hitori21@users.noreply.github.com>
2023-03-18 10:15:48 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import re
+from prompt_toolkit import HTML
 from prompt_toolkit.shortcuts import (
 	yes_no_dialog,
 	message_dialog,
@@ -6,105 +7,115 @@ from prompt_toolkit.shortcuts import (
 	radiolist_dialog
 )
 
-from config.metadata import Styles
+from config.metadata import Styles, Messages
 from lib.scraper import Kusonime
 from lib.download import Download
 
-def create_key_list2(data):
-	keys = data.keys()
-	result = []
-	for key in keys:
-		if isinstance(data[key], dict):
-			for sub_key in data[key]:
-				alias = sub_key.upper()
-				result.append((sub_key, alias))
+
+def create_sub_key_list(data):
+	sub_keys = []
+	for key, value in data.items():
+		if isinstance(value, dict):
+			sub_keys.extend([(sub_key, sub_key.upper()) for sub_key in value])
 		else:
-			result.append((key, key.upper()))
-	return result
+			sub_keys.append((key, key.upper()))
+	return sub_keys
+
 
 def create_key_list(data):
-	result = []
-	for key in data:
-		result.append((key, key.upper()))
-	return result
+	return [(key, key.upper()) for key in data]
 
-def isURL(url):
+
+def is_valid_url(url):
 	pattern = re.compile(
-		r'^https?://' 
-		r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' 
-		r'kusonime|'  
-		r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' 
-		r'(?::\d+)?'  
-		r'(?:/?|[/?]\S+)$', re.IGNORECASE)  
+		r'^https?://'
+		r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'
+		r'kusonime|'
+		r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
+		r'(?::\d+)?'
+		r'(?:/?|[/?]\S+)$', re.IGNORECASE)
 	return bool(pattern.match(url))
 
-def cancel(kata: str = "Sayonara!"):
+
+def display_goodbye_message(message="Sayonara!"):
 	message_dialog(
-		title="KusoDL V.0.1",
-		text=f"{kata}\n\nTerimakasih sudah menggunakan!\nSilahkan kunjungi profile author di URL dibawah\nhttps://arzxh.netlify.app",
+		title=Messages.title,
+		text=HTML(f"{message}\n\n{Messages.bye_text}"),
 		style=Styles.Retro
 	).run()
 
-def ddl(data, tup: tuple):
-	result = radiolist_dialog(
-	title="KusoDL V.0.1",
-	text="Pilih file hosting dari file yang ingin diunduh.\nDisarankan menggunakan Google Drive.",
-	values=tup,
-	style=Styles.Retro
+
+def download_episode(data, hosts):
+	selected_host = radiolist_dialog(
+		title=Messages.title,
+		text=Messages.host_text,
+		values=hosts,
+		style=Styles.Retro
 	).run()
-	if result:
+
+	if selected_host:
 		try:
-			data = data[result]
-			Download(data)
-			cancel("Success mengunduh file!\nSilahkan periksa folder /tmp untuk mengekstrak file.")
+			host_data = data[selected_host]
+			result = Download(host_data)
+			if result == True:
+				display_goodbye_message("Berhasil mengunduh file!\nFile akan tersimpan di folder tmp")
+			elif result == False:
+				display_goodbye_message("Operasi dibatalkan oleh User.")
 		except Exception as e:
-			cancel(f"Maaf, terjadi kesalahan!\nSebagai alternatif lain, silahkan buka url berikut\n\n{data[result]}")
+			display_goodbye_message(f"Terjadi kesalahan saat mengunduh file!\n<ansired>{str(e)}</ansired>\nSilahkan copy URL dibawah untuk mengunduhnya secara manual di browser.\n<ansigreen>{data[selected_host]}</ansigreen>")
 	else:
-		cancel("Operasi dibatalkan oleh user!")
-	
-def get_res(data, tup: tuple):
-	result = radiolist_dialog(
-	title="KusoDL V.0.1",
-	text="Pilih resolusi video yang ingin kamu unduh.",
-	values=tup,
-	style=Styles.Retro
+		display_goodbye_message("Operasi dibatalkan oleh User.")
+
+
+def get_episode_resolution(data, resolutions):
+	selected_resolution = radiolist_dialog(
+		title=Messages.title,
+		text=Messages.res_text,
+		values=resolutions,
+		style=Styles.Retro
 	).run()
-	if result:
+
+	if selected_resolution:
 		try:
-			data = data.get_url(result)
-			hosts = create_key_list2(data)
-			ddl(data, hosts)
+			resolution_data = data.get_url(selected_resolution)
+			hosts = create_sub_key_list(resolution_data)
+			download_episode(resolution_data, hosts)
 		except Exception as e:
-			cancel("Maaf, terjadi kesalahan.")
+			display_goodbye_message("Terjadi kesalahan.\n<ansired>{str(e)}</ansired>")
 	else:
-		cancel("Operasi dibatalkan oleh user!")
-	
-def start():
+		display_goodbye_message("Operasi dibatalkan oleh User.")
+
+
+def start_downloading():
 	url = input_dialog(
-		title="KusoDL V.0.1",
-		text="Masukkan URL anime dari website Kusonime\nhttps://kusonime.com",
+		title=Messages.title,
+		text=Messages.input_text,
 		style=Styles.Retro
 	).run()
-	if isURL(url):
-		try:
-			data = Kusonime(url.strip())
-			keys = create_key_list(data.all_res)
-			get_res(data, keys)
-		except Exception as e:
-			cancel("Maaf, Anime tidak bisa diunduh!")
-	else:
-		cancel("Maaf, URL yang kamu masukkan tidak valid!")
 
-def main():
+	if is_valid_url(url):
+		try:
+			kusonime_data = Kusonime(url.strip())
+			resolutions = create_key_list(kusonime_data.all_res)
+			get_episode_resolution(kusonime_data, resolutions)
+		except Exception as e:
+			display_goodbye_message("Terjadi kesalahan saat mengambil informasi dari URL yang diberikan.\n<ansired>{str(e)}</ansired>")
+	else:
+		display_goodbye_message("Tautan yang kamu berikan tidak valid, coba periksa kembali tautan kamu.")
+
+
+def run_program():
 	result = yes_no_dialog(
-		title="KusoDL V.0.1",
-		text="Selamat datang di Kusonime Downloader!\nSilahkan pilih 'Yes' untuk melanjutkan.",
+		title=Messages.title,
+		text=Messages.intro_text,
 		style=Styles.Retro
 	).run()
+
 	if result:
-		start()
+		start_downloading()
 	else:
-		cancel("Operasi dibatalkan oleh user!")
+		display_goodbye_message("Operasi dibatalkan oleh User.")
+
 
 if __name__ == "__main__":
-	main()
+	run_program()
